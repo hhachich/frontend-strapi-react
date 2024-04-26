58ef6dca964644f398e49c05df186c78c7f23819
hhachich <h.hachichou@gmail.com>
2024-04-26 13:07:21 +0200

    mise en place des hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { fetchApi } from "./fetchApi";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [categorys, setCategorys] = useState([]);
+
   const fetchProducts = async () => {
     const res = await fetchApi.get("/products?populate=*");
-    console.log(res.data.data);
+    setProducts(res.data.data);
   };
-  fetchProducts();
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   const fetchCategorys = async () => {
     const res = await fetchApi.get("/categories?populate=*");
-    console.log(res.data.data);
+    setCategorys(res.data.data);
   };
-  fetchCategorys();
+  useEffect(() => {
+    fetchCategorys();
+  }, []);
+
   return (
     <>
       <p>Hello</p>
