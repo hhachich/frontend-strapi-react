5a539ba65cb70e7a7559265151dd7c843cc619fc
hhachich <h.hachichou@gmail.com>
2024-04-26 12:25:27 +0200

    mise en place d'axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  // pour ajouter les relation comme les images on ajoute le populate
+  const fetchPoducts = async()=>{
+    const res = await axios.get(process.env.REACT_APP_API_URL+"/products?populate=*",{
+      headers:{
+        Authorization : "bearer " + process.env.REACT_APP_API_TOKEN
+      }
+    })
+    console.log(res.data.data)
+  }
+  fetchPoducts()
   return (
     <>
         <p>Hello</p>
